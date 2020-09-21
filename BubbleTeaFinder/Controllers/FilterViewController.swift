cb1c448f5de4cd4f8007ac7aea078b740a0bf7d0
Armando Carrillo <ingcompuarmando@gmail.com>
2020-09-21 08:15:08 -0500

    Returning a count

--- BubbleTeaFinder/Controllers/FilterViewController.swift
+++ BubbleTeaFinder/Controllers/FilterViewController.swift
@@ -1,32 +1,7 @@
-/// Copyright (c) 2019 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import UIKit
+import CoreData
 
 class FilterViewController: UITableViewController {
 
@@ -50,11 +25,24 @@ class FilterViewController: UITableViewController {
   @IBOutlet weak var nameZASortCell: UITableViewCell!
   @IBOutlet weak var distanceSortCell: UITableViewCell!
   @IBOutlet weak var priceSortCell: UITableViewCell!
+  
+  //MARK: - Properties
+  var coreDataStack : CoreDataStack!
+  //count for the first price category filter
+  lazy var cheapVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K == %@", #keyPath(Venue.priceInfo.priceCategory), "$")
+  } ()
+  //count for the second price category filter
+  lazy var moderateVenuePredicate: NSPredicate = {
+    return NSPredicate(format: "%K == %@", #keyPath(Venue.priceInfo.priceCategory), "$$")
+  }()
 
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
+    populateCheapVenueCountLabel()
+    populateModerateVenueCountLabel()
   }
 }
 
@@ -71,5 +59,41 @@ extension FilterViewController {
 
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
+  }
+}
+
+//MARK: - Helper methods
+
+extension FilterViewController {
+  func populateCheapVenueCountLabel() {
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = cheapVenuePredicate
+    
+    do {
+      let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+      
+      let count = countResult.first!.intValue
+      let pluralized = count == 1 ? "place" : "places"
+     firstPriceCategoryLabel.text = "\(count) bubble tea \(pluralized)"
+    } catch let error as NSError {
+      print("Count not fetched \(error), \(error.userInfo)")
+    }
+  }
+  
+  func populateModerateVenueCountLabel() {
+    let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Venue")
+    fetchRequest.resultType = .countResultType
+    fetchRequest.predicate = moderateVenuePredicate
+    
+    do {
+      let countResult = try coreDataStack.managedContext.fetch(fetchRequest)
+      
+      let count = countResult.first!.intValue
+      let pluralized = count == 1 ? "place" : "places"
+      secondPriceCategoryLabel.text = "\(count) bubble tea \(pluralized)"
+    } catch let error as NSError {
+      print("count not fetched \(error), \(error.userInfo)")
+    }
   }
 }
